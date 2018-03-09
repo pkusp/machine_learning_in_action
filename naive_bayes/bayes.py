157a86c11c5f5545aa9c9e08497dd363d14290b3
pkusp <pkusp@outlook.com>
2018-03-09 06:28:05 -0800

    <init>: naive bayes init

---
+++ naive_bayes/bayes.py
@@ -0,0 +1,13 @@
+# 2018.03.09
+# @auther: pkusp
+# description: machine learning in action
+
+def loadDataSet():
+	postingList = [['my','dog','has','flea',\
+					'problems','help','please'],
+					['maybe','not','take','him',\
+					'stupid']]
+	classVec = [0,1]
+	return postingList,classVec
+
+def 
